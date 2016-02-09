3e4f001471239abeb3d9059a97f67123782fbb0a
jaigoff <legoff_jeremy@yahoo.fr>
2016-02-09 14:08:26 +0100

    Add Month and localisation

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 static Window *s_window;
 static TextLayer *s_textlayer_hour;
 static TextLayer *s_textlayer_min;
+static TextLayer *s_textlayer_month;
 static Layer *s_canvas_layer_battery;
 static int16_t ibatterysize;
 static uint8_t ubatterycharge;
@@ -53,7 +54,12 @@ static void battery_handler(BatteryChargeState new_state) {
   layer_mark_dirty(s_canvas_layer_battery);
 }
 
-
+//Update Month Label
+static void updateMonth(struct tm *tick_time){
+    static char s_monthbuffer[18];
+    strftime(s_monthbuffer, sizeof(s_monthbuffer),"%B" , tick_time);
+    text_layer_set_text(s_textlayer_month, s_monthbuffer); 
+}
 
 //Update Hour Label
 static void updateHour(struct tm *tick_time){
@@ -92,10 +98,14 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   {
     updateHour(tm_tick_time);
   }
+  if(units_changed&MONTH_UNIT){
+    updateMonth(tm_tick_time);
+  }
   if(units_changed==0)//first load
   {
     updateMin(tm_tick_time);
     updateHour(tm_tick_time);
+    updateMonth(tm_tick_time);
   }
   //APP_LOG(APP_LOG_LEVEL_DEBUG,"TimeUnit END %d",units_changed);
 }
@@ -114,7 +124,7 @@ static void main_window_load(Window *window) {
   text_layer_set_text_color(s_textlayer_hour, C_COLOR_TEXT_HOUR);
   text_layer_set_text(s_textlayer_hour, "HH");
   text_layer_set_font(s_textlayer_hour, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_textlayer_hour, GTextAlignmentLeft);
+  text_layer_set_text_alignment(s_textlayer_hour, GTextAlignmentRight);
   
   
   
@@ -124,14 +134,20 @@ static void main_window_load(Window *window) {
   text_layer_set_text_color(s_textlayer_min, C_COLOR_TEXT_MIN);
   text_layer_set_text(s_textlayer_min, "00");
   text_layer_set_font(s_textlayer_min, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_textlayer_min, GTextAlignmentLeft);
+  text_layer_set_text_alignment(s_textlayer_min, GTextAlignmentRight);
   
   
+  s_textlayer_month=text_layer_create(GRect(PBL_IF_ROUND_ELSE(bounds.size.w-59,bounds.size.w-65),(bounds.size.h/3)*2 , 65 , bounds.size.h/9 ));
+  text_layer_set_background_color(s_textlayer_month, GColorRed);
+  text_layer_set_text_color(s_textlayer_month, GColorWhite);
+  text_layer_set_text(s_textlayer_month, "MM");
+  text_layer_set_font(s_textlayer_month, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  text_layer_set_text_alignment(s_textlayer_month, GTextAlignmentCenter);
   //add text to the window
   
   layer_add_child(window_layer, text_layer_get_layer(s_textlayer_hour));
   layer_add_child(window_layer, text_layer_get_layer(s_textlayer_min));
-  
+  layer_add_child(window_layer, text_layer_get_layer(s_textlayer_month));
   
   // Create Layer
   s_canvas_layer_battery = layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
@@ -146,7 +162,7 @@ static void main_window_unload(Window *window) {
   //destroy time
   text_layer_destroy(s_textlayer_hour);
   text_layer_destroy(s_textlayer_min);
-  
+  text_layer_destroy(s_textlayer_month);
     // Destroy Layer
   layer_destroy(s_canvas_layer_battery);
 }
@@ -158,7 +174,9 @@ static void init(){
   C_COLOR_TEXT_MIN=GColorWhite;
   C_COLOR_BACKGROUNG_HOUR=GColorDarkGray;
   C_COLOR_BACKGROUNG_MIN=GColorDarkGray;
-
+  //init locale
+  setlocale(LC_ALL, i18n_get_system_locale());
+  APP_LOG(APP_LOG_LEVEL_INFO,"Local %s", i18n_get_system_locale());
   s_window=window_create();
   
   // Set handlers to manage the elements inside the Window
